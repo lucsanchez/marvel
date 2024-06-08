7a650c86fada411717c4b0d3337407a4affe06d6
lucsanchez <luc.sanchez@hotmail.com>
2024-06-08 10:12:47 -0300

    feat: dev and prod modes for assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,65 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "node:path";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "@/ui/styles/mixins";'
+
+export default defineConfig(({ command, mode }) => {
+  if (command === "build") {
+    return {
+      plugins: [react()],
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src")
+        }
+      },
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: '@import "@/ui/styles/mixins";'
+          }
+        }
+      },
+      build: {
+        minify: "terser",
+        terserOptions: {
+          compress: {
+            drop_console: true
+          }
+        },
+        rollupOptions: {
+          output: {
+            manualChunks: undefined
+          }
+        }
       }
-    }
+    };
+  } else {
+    return {
+      plugins: [react(), cssInjectedByJsPlugin()],
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src")
+        }
+      },
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: '@import "@/ui/styles/mixins";'
+          }
+        }
+      },
+      build: {
+        minify: false,
+        rollupOptions: {
+          output: {
+            manualChunks: {
+              vendor: ["react", "react-dom"]
+            }
+          }
+        }
+      }
+    };
   }
 });
