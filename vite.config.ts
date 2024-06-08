790171c102e8fb04d7167e9558b3170529937174
lucsanchez <luc.sanchez@hotmail.com>
2024-06-08 12:38:39 -0300

    fix: removed unused import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "node:path";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 
